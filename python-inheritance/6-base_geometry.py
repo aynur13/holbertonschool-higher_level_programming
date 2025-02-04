2a7810cb1b37d51951f075082e5d0f77702ad46f
aynur13 <aynurmusayeva2007@gmail.com>
2025-02-04 09:10:37 +0000

    fix

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry
+"""
+class BaseGeometry:
+    """
+    still empty, area not implemented
+    """
+    # public instance method
+    def area(self):
+        raise Exception("area() is not implemented")
