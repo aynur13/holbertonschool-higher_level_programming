bd74c10bee92dcbc03ae2899afac8cc90f54821d
aynur13 <aynurmusayeva2007@gmail.com>
2025-02-04 09:28:18 +0000

    fix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -7,6 +7,7 @@ module with class BaseGeometry
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
     def __init__(self, width, height):
