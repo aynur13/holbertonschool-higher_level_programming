73eb3c2cfd573c61e7abae48fc4f8a2082b400c6
aynur13 <aynurmusayeva2007@gmail.com>
2025-02-04 09:34:42 +0000

    fix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,13 +16,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
-def area(self):
+    def area(self):
         """Method to redefine a area method in the parent class"""
         
         return self.__width * self.__height
-    
-def __str__(self):
+    def __str__(self):
         """__str__ method for return the next string"""
         
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
