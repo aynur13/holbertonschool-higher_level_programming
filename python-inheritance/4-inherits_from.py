79a5eeb8aa69b42c4857186cc5481e236c1b1cb7
aynur13 <aynurmusayeva2007@gmail.com>
2025-02-04 09:06:44 +0000

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """
 Module to check if an object is an instance of a class that inherited
 (directly or indirectly) from the specified class.
 """
+
 
 def inherits_from(obj, a_class):
     """Returns True if the object is an instance of a class that inherited
