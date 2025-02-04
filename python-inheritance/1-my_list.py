d484723b2db5749c5acbad88e8670fc652708987
aynur13 <aynurmusayeva2007@gmail.com>
2025-02-04 08:25:00 +0000

    fix

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for custom list (inherits from list)
+"""
+class MyList(list):
+    """Custom list with sorted print and no infinity values."""
+    
+    def print_sorted(self):
+        """Prints the list sorted, excluding infinity values."""
+        print(sorted(x for x in self if x not in [float('inf'), float('-inf')]))
+
+    def append(self, item):
+        """Appends item, raises error if it's infinity."""
+        if item in [float('inf'), float('-inf')]:
+            raise ValueError("Cannot append infinity values.")
+        super().append(item)
