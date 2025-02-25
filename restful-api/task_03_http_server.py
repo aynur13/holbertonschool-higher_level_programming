32f70e2b7fd2c4cfdaeb051777b4e58c92c8f77c
aynur13 <aynurmusayeva2007@gmail.com>
2025-02-25 07:48:39 +0000

    fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,39 +1,35 @@
-import http.server
-import json
-import socketserver
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-class NewHandler(http.server.BaseHTTPRequestHandler):
+class CustomHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!')
-
+            self.wfile.write(b"Root endpoint")
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data = {
-                "name": "John",
-                "age": 30,
-                "city": "New York"
-            }
-            self.wfile.write(json.dumps(data).encode())
-
+            self.wfile.write(b'{"data": "Some data"}')
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(b"OK")
-
+            self.wfile.write(b'{"status": "OK"}')
         else:
+            # Handle undefined endpoints by sending a custom response
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write('404 Not Found'.encode())
+            self.wfile.write(b"Endpoint not found")
 
-
-with socketserver.TCPServer(("", 8000), NewHandler) as httpd:
-    print(f"Serving at port {8000}")
+def run(server_class=HTTPServer, handler_class=CustomHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}')
     httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
+
